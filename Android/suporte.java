90220c915b5801cab3d1e530b9c15c8bf8bcb9e1
yKyubi <75593445+yKyubi@users.noreply.github.com>
2021-02-12 15:18:44 -0300

    Update suporte.java

--- Android/suporte.java
+++ Android/suporte.java
@@ -1,9 +1,9 @@
 /*
- * Classname             (darkbot3.0)
+ * Classname             (Izuk4Bot2.0)
  *
- * Version information   (3.0)
+ * Version information   (2.0)
  *
  * Date                  (06/12,15:35)
  *
- * author                (Dark YT)
+ * author                (𝑴𝑰𝑫𝑶𝑹𝑰𝒀𝑨𝑲𝑲𝑲𝑲𝑲𝑲𝑲)
  * Copyright notice      (bot com suporte a Android/Java)
